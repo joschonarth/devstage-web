92f05de5fedb46ea548e2d728def9db22fc1e75a
joschonarth <joschonarth@gmail.com>
2026-03-03 18:34:52 -0300

    feat: create InviteLinkInput component

---
+++ src/app/invite/components/invite-link-input.tsx
@@ -0,0 +1,22 @@
+import { Copy, Link } from 'lucide-react'
+import { IconButton } from '@/components/icon-button'
+import { InputField, InputIcon, InputRoot } from '@/components/input'
+
+export function InviteLinkInput() {
+  return (
+    <InputRoot>
+      <InputIcon>
+        <Link className="size-5" />
+      </InputIcon>
+
+      <InputField
+        readOnly
+        defaultValue="http://localhost:3000/invite/1e092usid1092jd1jaisdjoa"
+      />
+
+      <IconButton className="-mr-2">
+        <Copy className="size-5" />
+      </IconButton>
+    </InputRoot>
+  )
+}
