be5a3cb543a3fdfd62b7e085cbe8e77aa571373b
joschonarth <joschonarth@gmail.com>
2026-03-02 21:35:04 -0300

    feat: create Input component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
+import { ArrowRight, Copy, Mail } from 'lucide-react'
+import { Button } from '@/components/button'
+import { IconButton } from '@/components/icon-button'
+import { InputField, InputIcon, InputRoot } from '@/components/input'
+
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+      <Button>
+        Enviar
+        <ArrowRight />
+      </Button>
+
+      <IconButton>
+        <Copy />
+      </IconButton>
+
+      <div>
+        <InputRoot>
+          <InputIcon>
+            <Mail className="size-5" />
+          </InputIcon>
+
+          <InputField />
+        </InputRoot>
+      </div>
     </main>
-  );
+  )
 }
